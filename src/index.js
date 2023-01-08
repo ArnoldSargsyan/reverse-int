a833d4db4c2391c4e9b0a67fcc55ef1cff6584b9
ArnoldSargsyan <64585186+ArnoldSargsyan@users.noreply.github.com>
2023-01-08 18:03:59 +0400

    #1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let nIsNegative = false;
+    if(n < 0){
+        nIsNegative = true;
+        n = -n;
+    }
+    let rev_numb = 0;
+    while (n != 0){
+        rev_numb = rev_numb * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return rev_numb;
 }
